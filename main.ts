05a481a7a42eef2ae37fe40f16ed39d516dadbd2
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-04-28 09:39:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+function tife4ll () {
+    basic.showString("BYE")
+    basic.showIcon(IconNames.Happy)
+}
+input.onButtonPressed(Button.A, function () {
+    Tife4l()
+})
+function Tife4l () {
+    basic.showString("I LUV TYLER THE CREATOR")
+    basic.showIcon(IconNames.Heart)
+}
+input.onButtonPressed(Button.B, function () {
+    tife4ll()
+})
+basic.forever(function () {
+	
+})
